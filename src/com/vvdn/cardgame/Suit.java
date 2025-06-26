f9a0d6ac42c9a3768086e34b12b82544665b2ad9
hardeepsinghk04 <131161941+hardeepsinghk04@users.noreply.github.com>
2025-06-26 15:06:50 +0530

    Made Card class immutable and fixed abbrev variable

--- src/com/vvdn/cardgame/Suit.java
+++ src/com/vvdn/cardgame/Suit.java
@@ -1,15 +1,18 @@
 package com.vvdn.cardgame;
 
 public enum Suit {
-    CLUB, DIAMOND, HEART, SPADE;
+    CLUB('♣'),
+    DIAMOND('♦'),
+    HEART('♥'),
+    SPADE('♠');
+
+    private final char symbol;
+
+    Suit(char symbol) {
+        this.symbol = symbol;
+    }
 
     public char getImage() {
-        switch (this) {
-            case CLUB: return '\u2663';    // ♣
-            case DIAMOND: return '\u2666'; // ♦
-            case HEART: return '\u2665';   // ♥
-            case SPADE: return '\u2660';   // ♠
-            default: return '?';
-        }
+        return symbol;
     }
 }
